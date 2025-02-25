f3281b72106af74ffda81e435a70de17d36c5894
dhanushramudri <ramudridhanush@gmail.com>
2025-02-26 00:13:52 +0530

    removed console msgs

--- frontend/src/context/DoctorContext.jsx
+++ frontend/src/context/DoctorContext.jsx
@@ -39,7 +39,7 @@ const DoctorContextProvider = (props) => {
       const { data } = await axios.get(backendUrl + "/api/doctor/profile", {
         headers: { dToken },
       });
-      console.log(data.profileData);
+      // console.log(data.profileData);
       setProfileData(data.profileData);
     } catch (error) {
       console.log(error);
